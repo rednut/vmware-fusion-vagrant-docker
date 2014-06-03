4fc800ab0daf92e1c38bd06446475af44c043e31
Rednut Infomatics <dotcomstu@gmail.com>
2014-06-04 00:28:19 +0100

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export VAGRANT_VMWARE_FUSION_APP="/Applications/VMware Fusion.app"
-export APT_CACHER=http://10.9.1.9:3142
+export APT_CACHER=http://10.0.0.9:3142
 
 #export DOCKER_IP=
 export DOCKER_PORT=4243
